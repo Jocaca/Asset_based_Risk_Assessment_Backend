2c7038ce50613f71d28042241d3cf05bb3d6e8ed
Jocaca <115770399+Jocaca@users.noreply.github.com>
2025-07-08 14:32:04 +0800

    fix bug

--- src/main/java/com/example/assetbasedriskassessmentplatformbackend/Service/AuditProjectHomeService.java
+++ src/main/java/com/example/assetbasedriskassessmentplatformbackend/Service/AuditProjectHomeService.java
@@ -5,11 +5,11 @@ import org.springframework.http.ResponseEntity;
 import java.util.Map;
 
 public interface AuditProjectHomeService {
-    ResponseEntity<Map<String, Object>> getAllProjects(int page, int size);
-    ResponseEntity<Map<String, Object>> getFilteredProjects(int page, int size, int status);
-    ResponseEntity<Map<String, Object>> getSearchProjects(int page, int size, String searchTerm);
+    ResponseEntity<Map<String, Object>> getAllProjects(int page, int size, int userId, int userLevel);
+    ResponseEntity<Map<String, Object>> getFilteredProjects(int page, int size, int userId, int userLevel, int statusCode);
+    ResponseEntity<Map<String, Object>> getSearchProjects(int page, int size, int userId, int userLevel, String searchTerm);
     ResponseEntity<Map<String, Object>> createProject(Map<String, String> request);
-    ResponseEntity<Map<String, Object>> projectsCount();
-    ResponseEntity<Map<String, Object>> filterProjectsCount(int status);
-    ResponseEntity<Map<String, Object>> searchProjectsCount(String searchTerm);
+    ResponseEntity<Map<String, Object>> projectsCount(int userLevel, int userId);
+    ResponseEntity<Map<String, Object>> filterProjectsCount(int statusCode, int userLevel, int userId);
+    ResponseEntity<Map<String, Object>> searchProjectsCount(String searchTerm, int userLevel, int userId);
 }
